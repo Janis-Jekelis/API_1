bb444afdb85561720ab468a7c1fc18649520510c
Janis <janisjekelis@gmail.com>
2023-10-19 19:17:56 +0300

    01

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 declare(strict_types=1);
 require_once "vendor/autoload.php";
 use App\CollectionOfUniversities;
+use League\CLImate\CLImate;
 $country=readline("Enter country: ");
 $search=json_decode(file_get_contents("http://universities.hipolabs.com/search?country=$country"));
-
+$formatText=new CLImate();
  if(!$search){
      exit("invalid country name:".PHP_EOL);
  }
    $results=new CollectionOfUniversities($search);
    foreach ($results->getUniversities() as $university){
-      echo "Name: ".$university->getName() . PHP_EOL;
+       $formatText->green()->inline("Name: "); $formatText->yellow($university->getName());
       foreach ($university->getWebPages() as $page){
-          echo "Web page: ".$page . PHP_EOL;
+       $formatText->green()->inline("Web page: "); echo $page . PHP_EOL;
       }
       echo PHP_EOL;
    }
